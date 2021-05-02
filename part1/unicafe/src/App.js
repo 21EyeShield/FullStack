21d4c622972238eb4822419653baa42c19aadd00
Taiga Aisaka <mtcart.contact@gmail.com>
2021-05-02 09:20:15 +0100

    unicafe step3 and 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,29 @@
 import React, { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Statistics = ({good, neutral, bad, all}) => {
+
+  if(all ===0){
+    return(
+      <div>
+        no feedback given
+      </div>
+    )
+  }
+  return( 
+    <div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {(good-bad)/all}</div>
+      <div>positive {(good * 100)/all} %</div>
+    </div>
+)
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
@@ -26,23 +47,18 @@ const App = () => {
     setAll(all + 1)
     setBad(bad + 1)
   }
-  
+
   return (
     <div>
       <div>
       <h1>give feed back</h1>
-      <Button handleClick ={handleGood} text ='good' />
-      <Button handleClick ={handleNeutral} text ='neutral' />
-      <Button handleClick ={handleBad} text ='bad' />
+        <Button handleClick ={handleGood} text ='good' />
+        <Button handleClick ={handleNeutral} text ='neutral' />
+        <Button handleClick ={handleBad} text ='bad' />
       </div>
       <div>
         <h1>statistics</h1>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {all}</div>
-        <div>average {(good-bad)/all}</div>
-        <div>positive {(good*100)/all} %</div>
+        <Statistics good ={good} neutral={neutral} bad={bad} all={all} />
       </div>
     </div>
   )
