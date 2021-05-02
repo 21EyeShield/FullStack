f114a578546318534677d59b05bbbc5652285897
Taiga Aisaka <mtcart.contact@gmail.com>
2021-05-02 09:02:00 +0100

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,19 +11,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+  const [all, setAll] = useState(0)
   const handleGood = () => {
+    setAll(all + 1)
     setGood(good + 1)
   }
 
   const handleNeutral = () => {
+    setAll(all + 1)
     setNeutral(neutral + 1)
   }
 
   const handleBad = () => {
+    setAll(all + 1)
     setBad(bad + 1)
   }
-
+  
   return (
     <div>
       <div>
@@ -37,6 +40,9 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {(good-bad)/all}</div>
+        <div>positive {(good*100)/all} %</div>
       </div>
     </div>
   )
