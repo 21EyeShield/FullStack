cf51c8bf6e79c4db5f42bd69bfff5de010bca969
Taiga Aisaka <mtcart.contact@gmail.com>
2021-05-09 12:54:19 +0100

    refractored

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
